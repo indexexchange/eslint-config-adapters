6279d9bd0354eca385ff80c546a5f63115db30b5
adapter-certification-eng <adapter-certification-eng@indexexchange.com>
2018-08-01 17:28:18 -0400

    Added adapter-specific rule overrides on top of base.

---
+++ index.js
@@ -0,0 +1,57 @@
+module.exports = {
+    'extends': [
+        'eslint-config-base/4.13.1/main',
+        'eslint-config-base/4.13.1/possible-errors',
+        'eslint-config-base/4.13.1/best-practices',
+        'eslint-config-base/4.13.1/strict-mode',
+        'eslint-config-base/4.13.1/variables',
+        'eslint-config-base/4.13.1/nodejs-and-commonjs',
+        'eslint-config-base/4.13.1/main/stylistic-issues'
+    ],
+
+    'parserOptions': {
+        'ecmaVersion': 5,
+
+        'sourceType': 'script',
+
+        'ecmaFeatures': {
+            'impliedStrict': false
+        }
+    },
+
+    'env': {
+        'browser': true,
+        'commonjs': true
+    },
+
+    'rules': {
+        'complexity': ['off'],
+        'no-implicit-globals': ['off'],
+        'no-inner-declarations': ['error', 'functions'],
+        'no-magic-numbers': ['off'],
+        'no-var': ['off'],
+        'spaced-comment': [
+            'error',
+            'always',
+            {
+                'line': {
+                    'markers': ['?'],
+                    'exceptions': ['/']
+                },
+                'block': {
+                    'markers': ['*', '?'],
+                    'balanced': true
+                }
+            }
+        ],
+        'valid-jsdoc': ['off'],
+        'require-jsdoc': ['off'],
+        'new-cap': ['off'],
+        'newline-per-chained-call': [
+            'error',
+            {
+                'ignoreChainWithDepth': 2
+            }
+        ]
+    }
+};
